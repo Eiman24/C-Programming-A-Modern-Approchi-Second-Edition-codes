fcbab818ec4d6c54917080eff13510fb83498bd6
Eiman24 <34124245+Eiman24@users.noreply.github.com>
2018-01-08 22:07:06 +0800

    a

--- Chapter 4/4p4.c
+++ Chapter 4/4p4.c
@@ -5,11 +5,11 @@ int main(void)
 	int a,b,c,d,e,f;
 	printf("enter a NO.:");
 	scanf("%d",&a);
-	f=a%8,
-	e=a/8%8,
-	d=a/8/8%8,
-	c=a/8/8/8%8,
-	b=a/8/8/8/8%8;
+	f = a%8,
+	e = a/8%8,
+	d = a/8/8%8,
+	c = a/8/8/8%8,
+	b = a/8/8/8/8%8;
 	printf("%d%d%d%d%d\n",b,c,d,e,f );
 
 	
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # C-Programming-A-Modern-Approchi-Second-Edition-codes
-practices &amp; programming in learning C Programming: A Modern Approchi, Second Edition
+practices & programming in learning C Programming: A Modern Approchi, Second Edition
